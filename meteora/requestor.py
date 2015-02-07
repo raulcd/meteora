923fde25706ac2d133aa5e15171bee808dc5ff1e
Bri <briannelmurphy@gmail.com>
2015-02-07 12:24:24 +0000

    Adding request pkg

--- meteora/requestor.py
+++ meteora/requestor.py
@@ -5,14 +5,13 @@ import requests
 def run_requests(url, num_requests):
     loop = asyncio.get_event_loop()
     future = []
-    for (i in range(num_requests)):
-    	future[i] = loop.run_in_executor(None, requests.get, url)
+    response = []
+    for i in range(num_requests):
+        future[i] = loop.run_in_executor(None, requests.get, url)
     
-    for (i in range(num_requests)):
-    	response[i] = yield from future[i]
-    	print(response[i].text)
-
-
+    for i in range(num_requests):
+        response[i] = yield from future[i]
+        print(response[i].text)
 
 
 class Requestor(object):
@@ -28,5 +27,5 @@ class Requestor(object):
 
     def start_requests(self):
         loop = asyncio.get_event_loop()
-		loop.run_until_complete(run_requests(self.url, self.number_of_requests))
+        loop.run_until_complete(run_requests(self.url, self.number_of_requests))
 
