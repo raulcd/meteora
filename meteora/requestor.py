2019a97c2b14c840ccf74eb3097431ec4cfbe5ba
Raul Cumplido <raulcumplido@gmail.com>
2015-02-07 14:03:24 +0000

    Refactor requests classes

--- meteora/requestor.py
+++ meteora/requestor.py
@@ -2,30 +2,6 @@ import asyncio
 import requests
 
 
-def do_request(url, num_requests):
-    responses = []
-    for i in range(num_requests):
-        responses.append(requests.get(url))
-    return responses
-
-
-@asyncio.coroutine
-def run_requests(url, num_requests, num_concurrent_users=1):
-    loop = asyncio.get_event_loop()
-    futures = []
-    responses = []
-    for i in range(num_concurrent_users):
-        futures.append(
-            loop.run_in_executor(
-                None, do_request, url, num_requests
-            )
-        )
-    for i in range(num_concurrent_users):
-        response = yield from futures[i]
-        responses.append(response)
-    return responses
-
-
 class Requestor(object):
     """
     Main class to generate requests.
@@ -40,11 +16,33 @@ class Requestor(object):
     def start_requests(self):
         loop = asyncio.get_event_loop()
         results = loop.run_until_complete(
-            run_requests(
+            self._run_requests(
                 self.url, self.number_of_requests
             )
         )
         self.results = Result(results)
+
+    def _do_request(self, url, num_requests):
+        responses = []
+        for i in range(num_requests):
+            responses.append(requests.get(url))
+        return responses
+
+    @asyncio.coroutine
+    def _run_requests(self, url, num_requests, num_concurrent_users=1):
+        loop = asyncio.get_event_loop()
+        futures = []
+        responses = []
+        for i in range(num_concurrent_users):
+            futures.append(
+                loop.run_in_executor(
+                    None, self._do_request, url, num_requests
+                )
+            )
+        for i in range(num_concurrent_users):
+            response = yield from futures[i]
+            responses.append(response)
+        return responses
 
 
 class Result(object):
